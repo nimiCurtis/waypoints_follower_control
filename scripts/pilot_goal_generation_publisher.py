d425fef75e284a4a788f6a052bf3aae539f1a967
nimrodCurtis <nimicu21@gmail.com>
2024-11-03 18:09:17 +0200

    update

--- scripts/pilot_goal_generation_publisher.py
+++ scripts/pilot_goal_generation_publisher.py
@@ -237,7 +237,7 @@ class BaseGoalGenerator:
 
         params = {
             "robot": rospy.get_param(self.node_name + "/robot", default="turtlebot"),
-            "model_name": rospy.get_param(self.node_name + "/model/model_name", default="pilot_bsz160_c6_ac3_gcp0.4_mdp0.0_ph162024-08-24_15-16-54"),
+            "model_name": rospy.get_param(self.node_name + "/model/model_name", default="pilot_bsz128_c1_ac1_gcp0.2_mdp0.0_ph16_2024-10-09_20-26-18"),
             "model_version": str(rospy.get_param(self.node_name + "/model/model_version", default="best_model")),
             "frame_rate": rospy.get_param(self.node_name + "/model/frame_rate", default=7),
             "pub_rate": rospy.get_param(self.node_name + "/model/pub_rate", default=10),
@@ -359,7 +359,7 @@ class GoalGenerator(BaseGoalGenerator):
         self.realtime_traj = RealtimeTraj()
         self.start_time = rospy.Time.now()
         
-        self.subgoal_gen = SubgoalsGen(threshold=0.5)
+        self.subgoal_gen = SubgoalsGen(threshold=1.5)
         self.subgoal_to_target = None
         
         
